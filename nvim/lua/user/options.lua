b121d1b30c5ca9ee053e576dd57a5c22af7db678
BaizaOP <zayn.baig@gmail.com>
2022-01-08 00:40:57 -0500

    lua-ified it by extrapolating the options to a dict and using a for-loop (EP 1-2)

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,37 +1,45 @@
 -- :help options
-vim.opt.cmdheight = 2
-vim.opt.completeopt = { "menuone", "noselect" } -- used for cmp
-vim.opt.conceallevel = 0
--- vim.opt.cursorline = true
-vim.opt.fileencoding = "utf-8"
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.mouse = "a"
-vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.showmode = false
-vim.opt.showtabline = 2
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.timeoutlen = 1000                       -- time to wait for keys in a mapping (ex "gcc")
-vim.opt.undofile = true
-vim.opt.updatetime = 300                        -- faster completion (4000ms default)
+local options = {
+  -- vim.opt.cursorline = true
+  cmdheight = 2,
+  completeopt = { "menuone", "noselect" }, -- used for cmp
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  termguicolors = true,
+  timeoutlen = 1000,                       -- time to wait for keys in a mapping (ex "gcc")
+  undofile = true,
+  updatetime = 300,                        -- faster completion (4000ms default),
 
--- make alphabetical for above, maybe with alt-j + alt-k keymaps?
-vim.opt.expandtab = true                        -- convert tabs to spaces
-vim.opt.shiftwidth = 2                          -- 2 tab width
-vim.opt.tabstop = 2
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 2
-vim.opt.signcolumn = "yes"                      -- shows that line left of the line numbers
-vim.opt.wrap = false                            -- one long line of text
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- font used for graphical nvim
+  -- make alphabetical for above, maybe with alt-j + alt-k keymaps?
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- 2 tab width
+  tabstop = 2,
+  number = true,
+  relativenumber = true,
+  numberwidth = 2,
+  signcolumn = "yes",                      -- shows that line left of the line numbers
+  wrap = false,                            -- one long line of text
+  scrolloff = 8,
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- font used for graphical nvim
+}
 
 vim.opt.shortmess:append "c"                    -- see :help shortmess
+
+-- ooh nice, a for loop over a dictionary! \o/
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd "set iskeyword+=-"
