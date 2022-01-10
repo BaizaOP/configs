e1dc9fe6aadd71ccb52d9dd6819923bee5c964c0
BaizaOP <zayn.baig@gmail.com>
2022-01-10 08:48:28 -0500

    added treesitter and now colors look SO GOOD! (EP 8)

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -69,6 +69,12 @@ return packer.startup(function(use)
   -- Telescope
   use "nvim-telescope/telescope.nvim"
 
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
