7afeca10e66ccb59e27b10d4b63a21b2f3d2f804
BaizaOP <zayn.baig@gmail.com>
2022-01-12 10:14:45 -0500

    added NvimTree file explorer to nvim configuration, MUCH better than netrw (Branch 12)

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -17,8 +17,8 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<CR>", opts)
-
+-- keymap("n", "<leader>e", ":Lex 30<CR>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)                  -- Resize with arrows
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
