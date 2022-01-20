54a2dde896a2c46037e85eee802f556d115307c5
BaizaOP <zayn.baig@gmail.com>
2022-01-20 13:11:10 -0500

    broke powershell/user_profile into two powershell config files to be accessable to powershell 5 (fix this)

---
+++ powershell/ps7_profile.ps1
@@ -0,0 +1,18 @@
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf -- FIX THIS WHEN YOU GET A CHANCE!!!!!!!
+# Import-Module PSFzf
+# Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Prompt takes a LONG time, maybe we can do in pure ps1?
+# Import-Module posh-git
+# Import-Module oh-my-posh
+# Set-PoshPrompt pure
